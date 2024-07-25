37d788536830ba55d4463f0fad731aca56713650
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2024-07-25 16:43:11 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css';
+
+function ShowPokemon({pokemons, handleClick}) {
+  return (
+    <div>
+          {pokemons.map((pokemon) => {
+            return <button key={pokemon.id} onClick = {() => handleClick(pokemon)}><img src={pokemon.image} style = {{ height: 50, width: 50}}/>{pokemon.name}</button>;
+          })}
+    </div>
+  )
+}
+
+export default function App() {
+  const [pokemons, setPokemons] = useState([])
+  const [team, setTeam] = useState([])
+  const [offset, setOffSet] = useState(20)
+  const limit = 20
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`)
+        .then(res => res.json())
+        .then(json => {
+          const pokemonList = json.results.map(pokemon => 
+            fetch(pokemon.url)
+              .then(res => res.json())
+              .then(data => ({
+                  id: data.id,
+                  name: pokemon.name,
+                  image: data.sprites.front_default,
+              }))
+          )
+
+          Promise.all(pokemonList)
+            .then(pokemonData => {
+              setPokemons(pokemonData)
+            });
+          })
+  }, [offset]);
+  
+  function MakeTeams(pokemon) {
+    if (team.length > 4) {
+      alert("Past limit")
+    } else {
+      setTeam(prevTeam => [...prevTeam, pokemon]);
+    }
+  }
+
+  function AdjustTeams(pokemonToRemove) {
+    setTeam((prevTeam) => {
+      const indexToRemove = prevTeam.findIndex(
+        (pokemon) => pokemon.id === pokemonToRemove.id
+      );
+
+      if (indexToRemove !== -1) {
+        return [
+          ...prevTeam.slice(0, indexToRemove),
+          ...prevTeam.slice(indexToRemove + 1),
+        ];
+      }
+
+      return prevTeam;
+    });
+  }
+
+  function NextPage() {
+    setOffSet(prevOffSet => prevOffSet + limit)
+  }
+
+  function PreviousPage() {
+    if (offset === 20) {
+      alert("No more pokemon")
+    } else {
+      setOffSet(prevOffSet => prevOffSet - limit)
+    }
+  }
+
+  return (
+    <>
+      <h1>Pokemon Team Builder</h1>
+      <h2>Choose 5</h2>
+      <ShowPokemon pokemons={pokemons} handleClick={MakeTeams}/>
+      <br></br> 
+      <div className = "container">
+        <button onClick={PreviousPage}>Previous Page</button> 
+        <button onClick={NextPage}>Next Page</button>
+      </div>
+      <h3>My Team: </h3>
+      {team.map((pokemon, index) => (
+        <button key={`${pokemon.id}-${index}`} onClick = {() => AdjustTeams(pokemon)}><img src={pokemon.image} style = {{ height: 50, width: 50 }}/> Click to remove {pokemon.name}</button>
+      ))}
+    </>
+  )
+}
+
+
